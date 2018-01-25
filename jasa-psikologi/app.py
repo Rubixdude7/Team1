e8c8e048f865df794aa0b9b2e589fa426b1e11a2
Brandon Duke <bmduke1997@gmail.com>
2018-01-25 03:37:14 -0500

    role management

--- jasa-psikologi/app.py
+++ jasa-psikologi/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 import datetime
 from flask_sqlalchemy import SQLAlchemy
-from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
+from flask_user import login_required,roles_required, UserManager, UserMixin, SQLAlchemyAdapter
 from flask_user.forms import RegisterForm
 from flask_mail import Mail
-from wtforms import StringField, DateField
+from wtforms import StringField, DateTimeField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -12,6 +12,7 @@ app.config['SECRET_KEY'] = 'thisisasecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://bgrwfoetjnrliplh:GRShWRVNEtekUUFPP647rgrHZSjGghQFxWjv8uMuAax4C8aL8bUxQC8AyipdFoGw@9a6e80b2-e34b-41f3-bd8d-a871003e804d.mysql.sequelizer.com/db9a6e80b2e34b41f3bd8da871003e804d'
 app.config['CSRF_ENABLED'] = True
 app.config['USER_APP_NAME'] = 'Passion'
+app.config['USER_AFTER_REGISTER_ENDPOINT'] = 'user.login'
 app.config.from_pyfile('config.cfg')
 
 db = SQLAlchemy(app)
@@ -33,14 +34,35 @@ class User(db.Model, UserMixin):
     active = db.Column(db.Boolean(), nullable=False, server_default='0')
     first_name = db.Column(db.String(100), nullable=False, server_default='')
     last_name = db.Column(db.String(100), nullable=False, server_default='')
-    user_dob = db.column(db.Date)
+    # user_dob = db.column(db.DateTime)
+
+    # Relationships
+    roles = db.relationship('Role', secondary='user_roles',
+                            backref=db.backref('users', lazy='dynamic'))
+
+    # Define the Role data model
+
+
+class Role(db.Model):
+    id = db.Column('role_id', db.BigInteger(), primary_key=True)
+    name = db.Column('role_nm', db.String(50), unique=True)
+
+    # Define the UserRoles data model
+
+
+class UserRoles(db.Model):
+    id = db.Column('user_role_id', db.BigInteger(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.user_id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('role.role_id', ondelete='CASCADE'))
 
 
 class MyRegisterForm(RegisterForm):
     first_name = StringField('First name', validators=[DataRequired('First name is required')])
     last_name = StringField('Last name',  validators=[DataRequired('Last name is required')])
-    user_dob = DateField('Date of birth', format='%m/%d/%Y', validators=[DataRequired('Date of birth is required')])
+    # user_dob = DateTimeField('Date of birth', format='%Y-%m-%d %H:%M:%S')
+    # TODO fix date of birth, and add 18 yr validation
 
+# db.create_all()
 
 # Setup Flask-User
 db_adapter = SQLAlchemyAdapter(db, User)  # Register the User model
@@ -61,6 +83,13 @@ def index():
 def profile():
 
     return 'This is protected page'
+
+
+@app.route('/role')
+@roles_required('admin')
+def role():
+
+    return 'you have permission'
 
 
 if __name__ == '__main__':
