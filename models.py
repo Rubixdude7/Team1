a5c1ad1ffb09a2ae736e2ec7277cc906c6ad8032
Brandon <bmduke1997@gmail.com>
2018-02-01 12:12:13 -0500

    fix another spot from the merge

--- models.py
+++ models.py
@@ -48,7 +48,6 @@ class user(MySQLModel, flask_user.UserMixin):
     active = BooleanField()
     first_name = CharField()
     last_name = CharField()
-    user_dob = DateTimeField()
     void_ind = CharField(default='n')
 
     # For flask_user
