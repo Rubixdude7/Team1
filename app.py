0a4db80e4079921b7ad1963283755f320505140c
bsheph08 <bmshepherd08@gmail.com>
2018-02-01 11:50:12 -0500

    Merge branch 'dev' of C:\Users\Brody\PycharmProjects\Team1 with conflicts.

--- app.py
+++ app.py
@@ -1,82 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 import datetime
-from flask_sqlalchemy import SQLAlchemy
-from flask import request
-from flask_user import login_required, roles_required, UserManager, UserMixin, SQLAlchemyAdapter, current_user
-from flask_user import login_required, roles_required, UserManager, UserMixin, SQLAlchemyAdapter, current_user, \
-    user_registered
+from flask_user import login_required, roles_required, UserManager, UserMixin, current_user, user_registered
 from flask_user.forms import RegisterForm
 from flask_mail import Mail
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateTimeField, Form, SelectField, SubmitField
-from wtforms.validators import DataRequired
-from flask import redirect, url_for
-from models import *
 from wtforms import StringField, DateField
 from wtforms.validators import DataRequired, ValidationError
 from data import Children  # part of the dummy data. This and the other dummy data stuff can be deleted later
+import models
 import query
-from flask import flash, render_template, request, redirect
 
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'thisisasecret'
-# my doesnt work
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://vnqimdibqlqhqgap:pdCJFgqiUjR68iGphryN5g6sdHEf3Voi7WQYPHvTpCFuyxdE3xkvYuQhgkF6Hjo8@38b32831-bc8a-4cf6-acef-a878003849f7.sqlserver.sequelizer.com/db38b32831bc8a4cf6acefa878003849f7'
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://bgrwfoetjnrliplh:GRShWRVNEtekUUFPP647rgrHZSjGghQFxWjv8uMuAax4C8aL8bUxQC8AyipdFoGw@9a6e80b2-e34b-41f3-bd8d-a871003e804d.mysql.sequelizer.com/db9a6e80b2e34b41f3bd8da871003e804d'
-# Jason's database
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mgqmsvhuvgtovyte:Aqyg6kb6tqDJjNvvoJEDGqJv8xTytGnRm8L28MPrnQjztPMk3xupApKjNchFyKKU@42576e98-688b-4ab2-8226-a87601334c89.mysql.sequelizer.com/db42576e98688b4ab28226a87601334c89'
-# Brandon's database
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://bgrwfoetjnrliplh:GRShWRVNEtekUUFPP647rgrHZSjGghQFxWjv8uMuAax4C8aL8bUxQC8AyipdFoGw@9a6e80b2-e34b-41f3-bd8d-a871003e804d.mysql.sequelizer.com/db9a6e80b2e34b41f3bd8da871003e804d'
-app.config[
-    'SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # TODO make sure this is ok, this gets rid of the warning in the terminal
 app.config['CSRF_ENABLED'] = True
 app.config['USER_APP_NAME'] = 'Passion'
 app.config['USER_AFTER_REGISTER_ENDPOINT'] = 'user.login'
 app.config.from_pyfile('config.cfg')
 Children = Children()  # part of the dummy data file
 
-db = SQLAlchemy(app)
 mail = Mail(app)
-
-
-class User(db.Model, UserMixin):
-    id = db.Column('user_id', db.BigInteger, primary_key=True)
-
-    # User authentication information
-    username = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=False, server_default='')
-
-    # User email information
-    email = db.Column(db.String(255), nullable=False, unique=True)
-    confirmed_at = db.Column(db.DateTime())
-
-    # User information
-    active = db.Column(db.Boolean(), nullable=False, server_default='0')
-    first_name = db.Column(db.String(100), nullable=False, server_default='')
-    last_name = db.Column(db.String(100), nullable=False, server_default='')
-    user_dob = db.column('user_dob', db.String(10))
-
-    # Relationships
-    roles = db.relationship('Role', secondary='user_roles',
-                            backref=db.backref('users', lazy='dynamic'))
-
-    def is_active(self):
-        return self.active
-
-
-# Define the Role data model
-class Role(db.Model):
-    id = db.Column('role_id', db.BigInteger(), primary_key=True)
-    name = db.Column('role_nm', db.String(50), unique=True)
-
-
-# Define the UserRoles data model
-class UserRoles(db.Model):
-    id = db.Column('user_role_id', db.BigInteger(), primary_key=True)
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.user_id', ondelete='CASCADE'))
-    role_id = db.Column(db.Integer(), db.ForeignKey('role.role_id', ondelete='CASCADE'))
 
 
 class MyRegisterForm(RegisterForm):
@@ -93,7 +36,7 @@ class MyRegisterForm(RegisterForm):
 
 
 # Setup Flask-User
-db_adapter = SQLAlchemyAdapter(db, UserClass=User)  # Register the User model
+db_adapter = models.PeeweeAdapter(models.db, models.user)  # Register the User model
 user_manager = UserManager(db_adapter, app, register_form=MyRegisterForm)  # Initialize Flask-User
 
 # set up query class as db
@@ -103,10 +46,9 @@ querydb = query.query()
 # new user registered
 @user_registered.connect_via(app)
 def _after_register_hook(sender, user, **extra):
-    role = Role.query.filter_by(name="user").first()
-    user_role = UserRoles(user_id=user.id, role_id=role.id)
-    db.session.add(user_role)
-    db.session.commit()
+    role = models.role.get(models.role.role_nm == 'user')
+    user_role = models.user_roles(user=user, role=role)
+    user_role.save()
 
 
 @app.route('/')
@@ -207,11 +149,36 @@ def parent():
     return render_template('parent.html', children=Children)
 
 
+# methods Brody added (may not work '-__- )
+@app.route('/child')
+def child():
+    return render_template('child.html')
+
+
+@app.route('/child', methods=['post'])
+def addChild():
+
+    # Ask how to get current user id for parent ID
+
+    kid = models.child
+    kid.child_nm_fst = request.form['firstname']
+    kid.child_nm_lst = request.form['lastname']
+
+    return render_template('parent.html', children=Children)
+
+# End danger zone (Brody Land)
+
 @app.route('/admin')
 @roles_required('admin')
 def admin():
     users = querydb.getAllUsers()
-    return render_template('admin.html', users=users)
+    roles = list()
+    usersandroles = dict()
+    for u in users:
+        r = querydb.role(u.user_id)
+        roles.append(r)
+        usersandroles[u.email] = r
+    return render_template('admin.html', users=users, roles=roles, usersandroles=usersandroles)
 
 
 class RoleChangeForm(FlaskForm):
