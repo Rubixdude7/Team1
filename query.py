31a8cdffaa47e0980a51609ed2e5c02285f9311c
Gabe <porter235@live.marshall.edu>
2018-02-07 23:32:16 -0500

    Contact info can now be properly updated

--- query.py
+++ query.py
@@ -215,6 +215,26 @@ class query(object):
         c = db.child.select().where(db.child.user == user_id)
         return c
 
+    def exists(self, contact_id, field):
+        c = db.contact.select().where(db.contact.contact_id == contact_id)
+        if field == 'phone_no' and c.phone_no.exists():
+            return True
+        elif field == 'address_1' and c.address_1.exists():
+            return True
+        elif field == 'address_2' and c.address_2.exists():
+            return True
+        elif field == 'city' and c.city.exists():
+            return True
+        elif field == 'providence' and c.providence.exists():
+            return True
+        elif field == 'zip' and c.zip.exists():
+            return True
+        else:
+            return False
+
+
+
+
 
     def contactID(self, user_id):
         try:
