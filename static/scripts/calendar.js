b893a1fcca91f6ee52bcfafba10fd66c6b792d51
Charlie Murphy <cmsmurp00@gmail.com>
2018-03-11 05:03:20 -0400

    Comprehensive schedule page

--- static/scripts/calendar.js
+++ static/scripts/calendar.js
@@ -1,3 +1,113 @@
+// Useful functions for generating strips that show availability times.
+var calendarStripModule = (function() {
+    var mod = {};
+    
+    var compareStrips = function(a, b) {
+        return a.start - b.start;
+    };
+    
+    var Strip = function(start, end) {
+        this.start = start;
+        this.end = end;
+    };
+    
+    var StripGroup = function() {
+        this.strips = [];
+    };
+    
+    StripGroup.prototype.add = function(start, end) {
+        this.strips[this.strips.length] = new Strip(start, end);
+    };
+    
+    StripGroup.prototype.combine = function() {
+        // combine() - Combines adjacent and overlapping strips.
+        // Loop until we're done.
+        var keep = [];
+        
+        for (var i = 0; i < this.strips.length; i++) {
+            keep[i] = true;
+        }
+        
+        for (var i = 0; i < this.strips.length; i++) {
+            for (var j = 0; j < this.strips.length; j++) {
+                if (i != j && keep[i] && keep[j]) {
+                    var stripA = this.strips[i];
+                    var stripB = this.strips[j];
+                    
+                    if (stripA.start <= stripB.end && stripA.end >= stripB.start) {
+                        stripA.start = Math.min(stripA.start, stripB.start);
+                        stripA.end = Math.max(stripA.end, stripB.end);
+                        keep[j] = false;
+                    }
+                }
+            }
+        }
+        
+        var newStrips = [];
+        for (var i = 0; i < this.strips.length; i++) {
+            if (keep[i]) {
+                newStrips[newStrips.length] = this.strips[i];
+            }
+        }
+        
+        this.strips = newStrips;
+        
+        this.normalize();
+    };
+    
+    StripGroup.prototype.splitAtIntervals = function(interval) {
+        // splitAtIntervals() - Splits existing strips at intervals.
+        var done = false;
+        while (!done) {
+            done = true;
+            
+            var stripsToAppend = [];
+            
+            for (var i = 0; i < this.strips.length; i++) {
+                var strip = this.strips[i];
+                
+                if (Math.floor(strip.start / interval) != Math.floor(strip.end / interval)) {
+                    stripsToAppend[stripsToAppend.length] = new Strip((Math.floor(strip.start / interval)+1) * interval,
+                                                                      strip.end);
+                    done = false;
+                }
+            }
+            
+            this.strips = this.strips.concat(stripsToAppend);
+        }
+        
+        this.normalize();
+    };
+    
+    StripGroup.prototype.eachInRange = function(start, end, fn) {
+        for (var i = 0; i < this.strips.length; i++) {
+            var strip = this.strips[i];
+            if (strip.start >= start && strip.end <= end) {
+                fn(strip.start, strip.end);
+            }
+        }
+    }
+    
+    StripGroup.prototype.normalize = function() {
+        // Make sure starts are before ends
+        for (var i = 0; i < this.strips.length; i++) {
+            var strip = this.strips[i];
+            if (strip.start > strip.end) {
+                var temp = strip.start;
+                strip.start = strip.end;
+                strip.end = temp;
+            }
+        }
+        
+        // Sort
+        this.strips.sort(compareStrips);
+    }
+    
+    mod.StripGroup = StripGroup;
+    
+    return mod;
+})();
+
 var calendarModule = (function() {
     var mod = {};
     
@@ -55,6 +165,8 @@ var calendarModule = (function() {
         this.month = d.getMonth();
         this.year = d.getFullYear();
         
+        this.refreshButtons();
+        
         var cal = this;
         this.elem.find(".cal-month-label").fadeOut(400);
         this.elem.find(".cal-body").slideUp(400, function() {
@@ -68,6 +180,8 @@ var calendarModule = (function() {
         var d = new Date(this.year, this.month - 1, 1);
         this.month = d.getMonth();
         this.year = d.getFullYear();
+        
+        this.refreshButtons();
         
         var cal = this;
         this.elem.find(".cal-month-label").fadeOut(400);
@@ -101,6 +215,27 @@ var calendarModule = (function() {
             daysFromNextMonth = 7*6  - (daysFromPrevMonth + lastDayThisMonth.getDate());
         }
 
+                
+        var stripGroup = new calendarStripModule.StripGroup();
+        for (var cell = 0; cell < 7*6; cell++) {
+            for (var i = 0; i < this.avails.length; i++) {
+                if (this.avails[i]["weekday"] == cell % 7) {
+                    var time_st = this.avails[i]["time_st"];
+                    var time_end = this.avails[i]["time_end"];
+                    
+                    var start = cell*24*60*60 + time_st["hour"]*60*60 + time_st["minute"]*60 + time_st["second"];
+                    var end = cell*24*60*60 + time_end["hour"]*60*60 + time_end["minute"]*60 + time_end["second"];
+                    
+                    stripGroup.add(start, end);
+                }
+            }
+        }
+        
+        stripGroup.normalize();
+        stripGroup.combine();
+        stripGroup.splitAtIntervals(24*60*60);
+        
+        var cal = this;
         this.elem.find('.cal-day').each(function (index, dayElem) {
             dayElem = $(dayElem);
             
@@ -110,67 +245,32 @@ var calendarModule = (function() {
             } else {
                 var dayNum = (index - daysFromPrevMonth) + 1;
                 var html = "<div class=\"cal-day-number\">" + dayNum.toString() + "</div>";
-/*
-                // We're creating a little stripe that shows availabilities
-                var eventArray = []; // Contains time STARTs and time ENDs
-
-                // Add all STARTs and ENDs to an array
-                for (var a_i = 0; a_i < availabilities.length; a_i++) {
-                    if (availabilities[a_i].weekday == day) {
-                        eventArray[eventArray.length] = new TimeEvent(availabilities[a_i].time_st, "start");
-                        eventArray[eventArray.length] = new TimeEvent(availabilities[a_i].time_end, "end");
-                    }
-                }
-
-                // Sort by time
-                eventArray.sort(compareTimeEvent);
-
-                // Create the stripe
-                var SECONDS_IN_DAY = 24 * 60 * 60;
-                var in_avail_slot = false;
-                var depth = 0;
-                var currentSeconds = 0;
-                for (var ev_i = 0; ev_i < eventArray.length; ev_i++) {
-                    var event = eventArray[ev_i];
-
-                    if (event.start_or_end == "start")
-                        depth++;
-                    else
-                        depth--;
-
-                    if (in_avail_slot) {
-                        if (depth <= 0) {
-                            in_avail_slot = false;
-                            var s = event.time.toSeconds();
-                            var width = 100 * (s - currentSeconds) / SECONDS_IN_DAY;
-                            html += "<div class=\"cal-ok-slot\" aria-hidden=\"true\" style=\"width:" + width.toString() + "%;\"></div>";
-                            currentSeconds = s;
-                        }
-                    } else {
-                        if (depth > 0) {
-                            in_avail_slot = true;
-                            var s = event.time.toSeconds();
-                            var width = 100 * (s - currentSeconds) / SECONDS_IN_DAY;
-                            html += "<div class=\"cal-space-slot\" aria-hidden=\"true\" style=\"width:" + width.toString() + "%;\"></div>";
-                            currentSeconds = s;
-                        }
-                    }
-                }
-
-                var leftoverWidth = 100 * (SECONDS_IN_DAY - currentSeconds) / SECONDS_IN_DAY;
-                html += "<div class=\"cal-space-slot\" aria-hidden=\"true\" style=\"width:" + leftoverWidth.toString() + "%;\"></div>";
-*/
+                
+                var lastEnd = index*24*60*60;
+                stripGroup.eachInRange(index*24*60*60, (index+1)*24*60*60, function(start, end) {
+                    html += "<div class=\"cal-space-slot\" style=\"width: " + ((start - lastEnd) * 100 / (24*60*60)) + "%;\"></div>";
+                    html += "<div class=\"cal-ok-slot\" style=\"width: " + ((end - start) * 100 / (24*60*60)) + "%;\"></div>";
+                    
+                    lastEnd = end;
+                });
+                
+                html += "<div class=\"cal-space-slot\" style=\"width: " + (((index+1)*24*60*60 - lastEnd) * 100 / (24*60*60)) + "%;\"></div>";
+                
                 dayElem.html(html);
                 dayElem.attr("data-this-month", "true");
             }
         });
 
+        this.refreshButtons();
+    };
+    
+    Calendar.prototype.refreshButtons = function() {
         if (this.year == this.minYear && this.month == this.minMonth) {
             this.prevBtn.attr("disabled", "disabled");
         } else {
             this.prevBtn.removeAttr("disabled");
         }
-
+        
         if (this.year == this.maxYear && this.month == this.maxMonth) {
             this.nextBtn.attr("disabled", "disabled");
         } else {
