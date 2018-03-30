162c447c0a4b49850f1f32c61018106b9f4d92dc
Gabe <porter235@live.marshall.edu>
2018-03-30 04:05:57 -0400

    tweaks for future videoConf functionality

--- query.py
+++ query.py
@@ -930,7 +930,7 @@ class query(object):
     #
     #     data = json.dumps(payload).encode('ascii')
     #     token = query.generateToken(self, data)  # stores the token
-    #
+    #yep
     #     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
     #                'Content-Type': 'application/json',
     #                'Authorization': 'Bearer ' + token}
